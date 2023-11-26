d5bf12765dbf399463374f7918075e14596d2d4e
dashaun <dashaun@dashaun.com>
2023-11-25 21:30:41 -0600

    add micrometer, add zipkin

--- src/main/java/dev/dashaun/web/learning/Application.java
+++ src/main/java/dev/dashaun/web/learning/Application.java
@@ -1,7 +1,18 @@
 package dev.dashaun.web.learning;
 
+import io.lettuce.core.resource.ClientResources;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.observability.MicrometerTracingAdapter;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +21,21 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+}
+
+@RestController
+class HelloController {
+	private final StringRedisTemplate redis;
+	
+	HelloController(StringRedisTemplate redis) {
+		this.redis = redis;
+	}
+	
+	@GetMapping("/")
+	public String index(){
+		String time  = String.valueOf(System.currentTimeMillis());
+		String key = "LEARNING:" + time;
+		redis.opsForValue().set(key, time);
+		return key;
+	}
 }
--- src/test/java/dev/dashaun/web/learning/TestApplication.java
+++ src/test/java/dev/dashaun/web/learning/TestApplication.java
@@ -16,6 +16,12 @@ public class TestApplication {
 		return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
 	}
 
+	@Bean
+	@ServiceConnection(name = "openzipkin/zipkin")
+	GenericContainer<?> zipkinContainer() {
+		return new GenericContainer<>(DockerImageName.parse("openzipkin/zipkin:latest")).withExposedPorts(9411);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.from(Application::main).with(TestApplication.class).run(args);
 	}
