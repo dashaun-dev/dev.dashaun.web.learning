256f56841e3f074b6447ed4a706a4d33f7949716
dashaun <dashaun@dashaun.com>
2023-11-25 22:04:54 -0600

    add them from start.vaadin.com

--- src/main/java/dev/dashaun/web/learning/Application.java
+++ src/main/java/dev/dashaun/web/learning/Application.java
@@ -1,41 +1,23 @@
 package dev.dashaun.web.learning;
 
-import io.lettuce.core.resource.ClientResources;
-import io.micrometer.observation.ObservationRegistry;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.observability.MicrometerTracingAdapter;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
 @SpringBootApplication
-public class Application {
+@Theme(value = "dashaun")
+public class Application implements AppShellConfigurator {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-}
-
-@RestController
-class HelloController {
-	private final StringRedisTemplate redis;
-	
-	HelloController(StringRedisTemplate redis) {
-		this.redis = redis;
-	}
-	
-	@GetMapping("/")
-	public String index(){
-		String time  = String.valueOf(System.currentTimeMillis());
-		String key = "LEARNING:" + time;
-		redis.opsForValue().set(key, time);
-		return key;
-	}
 }
